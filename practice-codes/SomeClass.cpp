a1bbbc30b4e4f96ca2334fb2cedba1e21cbffd66
Maya Tabbah <mayatabbah7@gmail.com>
2023-08-29 09:22:56 -0400

    hi

---
+++ practice-codes/SomeClass.cpp
@@ -0,0 +1,19 @@
+//Implementation
+
+#include “SomeClass.hpp”
+SomeClass::SomeClass()
+{
+    //implementation here
+}
+int SomeClass::methodOne()
+{
+    //implementation here
+}
+bool SomeClass::methodTwo()
+{
+    //implementation here
+}
+bool SomeClass::methodThree(intsomeParameter)
+{
+    //implementation here
+}
---
+++ practice-codes/SomeClass.hpp
@@ -0,0 +1,19 @@
+//Interface
+
+#ifndef SOME_CLASS_HPP_
+#define SOME_CLASS_HPP_
+#include <somelibrary>
+#include “AnotherClass.hpp”
+class SomeClass
+{
+public:
+SomeClass(); //Constructor
+int methodOne();
+bool methodTwo();
+bool methodThree(int
+someParameter);
+private:
+int data_member_one_;
+bool data_member_two_;
+}; //end SomeClass
+#endif
