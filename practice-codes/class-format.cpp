9c92559764ab943360cf33be06b2b174beb30741
Maya Tabbah <mayatabbah7@gmail.com>
2023-08-29 09:59:48 -0400

    hi

---
+++ practice-codes/class-format.cpp
@@ -0,0 +1,9 @@
+class MyClass
+MyClass class_instance;
+string my_variable;
+string my_member_variable_;
+void myMethod();
+int MY_CONSTANT;
+
+//Members always end in underscore 
+//classes always start with capital
